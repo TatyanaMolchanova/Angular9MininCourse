6a8e5482f3f09aa22892e50e828d77a20f9e2ffe
Molchanova Tatyana <mtuspeh@gmail.com>
2020-06-18 08:25:45 +0300

    n commit

--- Typescript/index.js
+++ Typescript/index.js
@@ -3,3 +3,57 @@ var num = 42;
 var isActive = false;
 var strArray = ['h', 'e', 'l'];
 var numArray = [1, 1, 2, 3];
+//==========
+// function logInfo(name: string, age: number): void {
+// console.log(`Info: ${name}, ${age}`)
+// }
+// logInfo('Tatynana', 25)
+//==========
+function calc(a, b) {
+    if (typeof b === 'string')
+        b = +b;
+    return a + b;
+}
+// console.log(calc(2, 5))
+//==========
+var Server = /** @class */ (function () {
+    // constructor(name: string, ip: number) {
+    function Server(name, ip) {
+        this.name = name;
+        this.ip = ip;
+        // public name: string
+        // protected ip: number
+        this.status = 'working';
+        // this.name = name
+        // this.ip = ip
+    }
+    Server.prototype.turnOn = function () {
+        this.status = 'working';
+    };
+    Server.prototype.turnOff = function () {
+        this.status = 'offline';
+    };
+    Server.prototype.getStatus = function () {
+        return this.status;
+    };
+    Server.VERSION = '1.0.3';
+    return Server;
+}());
+var server = new Server('AWS', 1234);
+var user = {
+    name: 'Tanya',
+    age: 38,
+    logInfo: function () {
+        console.log(this.name + ' ' + this.age);
+    }
+};
+var User = /** @class */ (function () {
+    function User(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    User.prototype.sayHello = function () {
+        console.log(this.name + 'Hello!');
+    };
+    return User;
+}());
--- Typescript/index.ts
+++ Typescript/index.ts
@@ -4,3 +4,110 @@ let isActive: boolean = false
 
 let strArray: string[] = ['h', 'e', 'l']
 let numArray: Array<number> = [1, 1, 2, 3]
+
+//==========
+
+// function logInfo(name: string, age: number): void {
+    // console.log(`Info: ${name}, ${age}`)
+// }
+
+// logInfo('Tatynana', 25)
+
+//==========
+
+function calc(a: number, b: number | string): number {
+    
+    if (typeof b === 'string') b = +b
+    
+    return a + b
+}
+
+// console.log(calc(2, 5))
+
+//==========
+
+class Server {
+    static VERSION = '1.0.3'
+
+    // public name: string
+    // protected ip: number
+    
+    private status: string = 'working'
+
+    // constructor(name: string, ip: number) {
+    constructor(public name: string, protected ip: number) {
+        // this.name = name
+        // this.ip = ip
+    }
+
+    public turnOn() {
+        this.status = 'working'
+    }
+
+    protected turnOff() {
+        this.status = 'offline'
+    }
+
+    public getStatus(): string {
+        return this.status
+    }
+}
+
+const server: Server = new Server('AWS', 1234)
+
+//==========
+
+interface UserInterface {
+    name: string
+    age: number
+    logInfo: () => void // function than nothing returns - функция, которая ничего не возращает (void)
+    id?: any // not required ? - may be or may not
+}
+
+const user: UserInterface = {
+    name: 'Tanya',
+    age: 38,
+    logInfo() {
+        console.log(this.name + ' ' + this.age)
+    },
+    // id: 3
+}
+
+//==========
+
+// interface SayHello {
+//     sayHello: () => void
+// }
+
+// class User implements SayHello {
+//     constructor(private name: string, private age: number) {}
+
+//     sayHello() {
+//         console.log(this.name + 'Hello!')
+//     }
+// }
+
+//==========
+
+// const arr: number[] = [1, 2, 3, 4]
+const arr: Array<number> = [1, 2, 3, 4]
+// const arr2: Array<any> = [1, 2, 3, 4]
+// const arr3: Array<string> = [1, 2, 3, 4]
+
+//==========
+
+interface User {
+    id: number
+    name: string
+    age: number
+}
+
+const users: Array<User> = [
+    {id: 1, name: 'V', age: 2},
+    {id: 2, name: 'E', age: 23}
+]
+
+const users2: Array<User> = [
+    {id: 1, name: 'V', age: 2},
+    {id: 2, name: 'E', age: 23}
+]
